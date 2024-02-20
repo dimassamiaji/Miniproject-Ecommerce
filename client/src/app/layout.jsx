482e3fd8cfac50c23023ddec43e71a3e80986a6e
dimassamiaji <dimassamji@gmail.com>
2024-02-20 13:46:57 +0700

    update

---
+++ client/src/app/layout.jsx
@@ -0,0 +1,32 @@
+/** @format */
+
+import { Provider } from "react-redux";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { StoreProvider } from "@/redux/store";
+import ProtectedPage from "@/routes/protected-routes";
+import AuthProvider from "@/routes/auth-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "KickAvenue",
+  description: "Kickavenue",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <StoreProvider>
+      <html lang="en">
+        <head>
+          {/* <link rel="shortcut icon" href="fav.png" type="image/x-icon" /> */}
+        </head>
+        <body className={inter.className}>
+          <AuthProvider>
+            <ProtectedPage>{children}</ProtectedPage>
+          </AuthProvider>
+        </body>
+      </html>
+    </StoreProvider>
+  );
+}
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,12 +1,19 @@
+/** @format */
+"use client";
+import { useDispatch, useSelector } from "react-redux";
+import { FiShoppingCart } from "react-icons/fi";
+import { functionLogout } from "../redux/slices/userSlices";
+import UserIcon from "../assets/user.svg";
 import Link from "next/link";
 import Image from "next/image";
-import { FiShoppingCart } from "react-icons/fi";
-function FunctionComponent() {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function NavbarComponent() {
   const userSelector = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const logout = () => {
     dispatch(functionLogout());
   };
+
   return (
     <div
       className={`flex justify-around  py-5  border-gray-400 items-center m-auto  max-w-screen-2xl w-full  sticky top-0 bg-white`}
@@ -56,4 +63,52 @@ function FunctionComponent() {
     </div>
   );
 }
-export default FunctionComponent;
+export default NavbarComponent;
+
+export function NavbarAdminComponent() {
+  const userSelector = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const logout = () => {
+    console.log("test");
+    dispatch(functionLogout());
+  };
+
+  return (
+    <div className="sticky top-0">
+      <div className="flex justify-around  py-5  border-gray-400 items-center m-auto  max-w-screen-2xl w-full   bg-white">
+        {/* logo */}
+        <Link className="font-bold text-xl" href={"/admin"}>
+          Admin Dashboard
+        </Link>
+
+        {/* login and register */}
+        {userSelector?.id ? (
+          <div className="flex gap-3">
+            <div>
+              Welcome,
+              {userSelector?.first_name + " " + userSelector?.last_name}
+            </div>
+            <button
+              className="rounded-md border-gray-500 border  px-2"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-1 text-sm text-[#23A6F0] font-semibold items-center">
+            <Image src={UserIcon}></Image>
+            <Link className="" href={"/login"}>
+              Login
+            </Link>
+            /
+            <Link className=" " href={"/register"}>
+              Register
+            </Link>
+          </div>
+        )}
+      </div>
+      <hr />
+    </div>
+  );
+}
