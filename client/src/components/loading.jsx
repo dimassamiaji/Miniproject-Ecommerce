72fcb3277d4d35eb417e31ebb90b59bca8420fd9
dimassamiaji <dimassamji@gmail.com>
2024-02-21 11:12:24 +0700

    edit loading page

--- client/src/components/loading.jsx
+++ client/src/components/loading.jsx
@@ -2,8 +2,8 @@
 
 function LoadingPage() {
   return (
-    <div className=" flex w-screen h-screen font-bold justify-center items-center text-3xl">
-      BrandName{" "}
+    <div className=" flex w-screen h-screen font-bold justify-center items-center text-3xl bg-gradient-to-tr">
+      Gotix{" "}
     </div>
   );
 }
