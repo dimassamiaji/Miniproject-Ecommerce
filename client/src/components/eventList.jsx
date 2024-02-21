9f1ca00a4ef250821914b2cec01ae1aa28edf822
dimassamiaji <dimassamji@gmail.com>
2024-02-21 17:59:35 +0700

    update

--- client/src/components/eventList.jsx
+++ client/src/components/eventList.jsx
@@ -62,7 +62,7 @@ function EventListComponent() {
 
 export default EventListComponent;
 
-export function EventCard({ image_url, eventName, id, price }) {
+export function EventCard({ image_url, eventName, id, price, location }) {
   return (
     <Link className="flex flex-col" href={"/events/" + id}>
       <img
